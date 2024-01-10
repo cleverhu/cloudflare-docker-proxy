020f2ad790c3f34d4323ac4c6837875aa49cadcf
cleverhu <zhubai.hsp@xuelanyun.com>
2024-01-10 12:06:52 +0800

    change host
    Signed-off-by: cleverhu <zhubai.hsp@xuelanyun.com>

--- README.md
+++ README.md
@@ -27,11 +27,11 @@
    - add more records and modify the config as you need
    ```javascript
    const routes = {
-     "docker.libcuda.so": "https://registry-1.docker.io",
-     "quay.libcuda.so": "https://quay.io",
-     "gcr.libcuda.so": "https://k8s.gcr.io",
-     "k8s-gcr.libcuda.so": "https://k8s.gcr.io",
-     "ghcr.libcuda.so": "https://ghcr.io",
+     "docker.study-k8s.com": "https://registry-1.docker.io",
+     "quay.study-k8s.com": "https://quay.io",
+     "gcr.study-k8s.com": "https://k8s.gcr.io",
+     "k8s-gcr.study-k8s.com": "https://k8s.gcr.io",
+     "ghcr.study-k8s.com": "https://ghcr.io",
    };
    ```
 
--- src/index.js
+++ src/index.js
@@ -4,13 +4,12 @@ addEventListener("fetch", (event) => {
 });
 
 const routes = {
-  "docker.libcuda.so": "https://registry-1.docker.io",
-  "quay.libcuda.so": "https://quay.io",
-  "gcr.libcuda.so": "https://gcr.io",
-  "k8s-gcr.libcuda.so": "https://k8s.gcr.io",
-  "k8s.libcuda.so": "https://registry.k8s.io",
-  "ghcr.libcuda.so": "https://ghcr.io",
-  "cloudsmith.libcuda.so": "https://docker.cloudsmith.io",
+  "docker.study-k8s.com": "https://registry-1.docker.io",
+  "quay.study-k8s.com": "https://quay.io",
+  "gcr.study-k8s.com": "https://gcr.io",
+  "k8s-gcr.study-k8s.com": "https://k8s.gcr.io",
+  "k8s.study-k8s.com": "https://registry.k8s.io",
+  "ghcr.study-k8s.com": "https://ghcr.io",
 };
 
 function routeByHosts(host) {
